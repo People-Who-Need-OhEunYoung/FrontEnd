6c061a724d0d2362fe8c1c225c910fb2c90788db
Deokgi-Park <ejrrl6931@naver.com>
2024-06-30 22:59:25 +0900

    초기셋팅

---
+++ pokecode/src/App.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+function App() {
+  return (
+    <>
+      <AppWrapper>test</AppWrapper>
+    </>
+  );
+}
+const AppWrapper = styled.div`
+  color: red;
+`;
+export default App;
