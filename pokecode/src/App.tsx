a34f642e2027fdaf48b23390056362282134b4ba
Deokgi-Park <ejrrl6931@naver.com>
2024-06-30 23:18:00 +0900

    초기셋팅2

--- pokecode/src/App.tsx
+++ pokecode/src/App.tsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <>
+      <Header></Header>
       <AppWrapper>test</AppWrapper>
     </>
   );
