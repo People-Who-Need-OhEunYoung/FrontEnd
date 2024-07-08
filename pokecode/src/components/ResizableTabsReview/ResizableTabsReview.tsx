f222428d78c6660f9527d428e672037dd1e21125
Deokgi-Park <ejrrl6931@naver.com>
2024-07-08 09:04:45 +0900

    feat:공동에디터 포켓몬 드래그드랍

--- pokecode/src/components/ResizableTabsReview/ResizableTabsReview.tsx
+++ pokecode/src/components/ResizableTabsReview/ResizableTabsReview.tsx
@@ -1,11 +1,19 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import styled from 'styled-components';
-import { motion, useAnimation } from 'framer-motion';
+import {
+  motion,
+  useAnimation,
+  useDragControls,
+  useMotionValue,
+} from 'framer-motion';
 import { Pokemon } from '../../pages/UserMain/UserMain';
 import { TestSharedEditor } from '../TestSharedEditor';
 import background from '../../assets/images/background.jpg';
 import problem1011 from '../../assets/images/1011번-Fly-me-to-the-Alpha-Centauri.png';
 import terminal from '../../assets/images/터미널.png';
+import webrtc from '../../assets/images/webrtc.png';
+import { DesignedButton1 } from '../DesignedButton';
+
 const Container = styled.div`
   display: flex;
   align-items: center;
@@ -50,23 +58,22 @@ const Resizer = styled.div`
   }
 `;
 
-const ResizableTabsReview: React.FC = () => {
+const ResizableTabsReview = () => {
   const [width, setWidth] = useState<number>(25);
   const [width1, setWidth1] = useState<number>(25);
-  const [position, setPosition] = useState({
-    x: '50%',
-  });
-  const containerRef = useRef<HTMLDivElement | null>(null);
-  const controls = useAnimation();
-  const handleDivClick = (e: any) => {
-    const containerRect = e.currentTarget.getBoundingClientRect();
-    const offsetX: any = e.clientX - containerRect.left;
 
-    setPosition({ x: offsetX });
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const dragControls = useDragControls();
+  const animationControls = useAnimation();
+
+  const handleDragEnd = () => {
+    animationControls.start({
+      x: 0,
+      y: 0,
+      transition: { duration: 0.5 }, // 이동하는 시간 설정 (예: 0.5초)
+    });
   };
-  const getRandomPosition = () => ({
-    x: Math.random() - Math.random() * 200,
-  });
 
   const handleMouseDown = () => {
     document.addEventListener('mousemove', handleMouseMove);
@@ -106,33 +113,6 @@ const ResizableTabsReview: React.FC = () => {
     document.removeEventListener('mouseup', handleMouseUp1);
   };
 
-  const sleep = (ms: any) => new Promise((resolve) => setTimeout(resolve, ms));
-
-  useEffect(() => {
-    const animateRandomly = async () => {
-      while (true) {
-        // 무작위 위치로 애니메이션 시작
-        const newPosition = getRandomPosition();
-        await controls.start({
-          ...newPosition,
-          transition: { duration: 1 + Math.random() * 3 },
-        });
-
-        // 1초 동안 대기
-        await sleep(1000);
-
-        // 제자리 애니메이션 (사실상 이동이 없도록 함)
-        await controls.start({
-          x: newPosition.x,
-          transition: { duration: 1 + Math.random() * 3 },
-        });
-
-        // 다시 1초 동안 대기
-        await sleep(1000);
-      }
-    };
-    animateRandomly();
-  }, [controls]);
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -143,37 +123,164 @@ const ResizableTabsReview: React.FC = () => {
       exit={{ opacity: 0 }}
       style={{ position: 'relative', height: 'calc(100vh - 180px)' }}
     >
+      <motion.div
+        drag
+        dragControls={dragControls}
+        animate={animationControls} // 애니메이션 컨트롤 적용
+        style={{
+          position: 'fixed',
+          display: 'inline-block',
+          width: '4vw',
+          zIndex: 9999,
+          bottom: '30%',
+          right: 150,
+          transform: 'translateX(0px) translateY(0px) translateZ(0px)',
+          transition: '0.1s',
+        }}
+        className="pokemon"
+      >
+        <Pokemon
+          width={'100%'}
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/136.gif"
+        ></Pokemon>
+      </motion.div>
+      <motion.div
+        drag
+        dragControls={dragControls}
+        animate={animationControls} // 애니메이션 컨트롤 적용
+        style={{
+          position: 'fixed',
+          display: 'inline-block',
+          width: '4vw',
+          zIndex: 9999,
+          bottom: '30%',
+          right: 200,
+          transform: 'translateX(0px) translateY(0px) translateZ(0px)',
+          transition: '0.1s',
+        }}
+        className="pokemon"
+      >
+        <Pokemon
+          width={'100%'}
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/133.gif"
+        ></Pokemon>
+      </motion.div>
+      <motion.div
+        drag
+        dragControls={dragControls}
+        animate={animationControls} // 애니메이션 컨트롤 적용
+        style={{
+          position: 'fixed',
+          display: 'inline-block',
+          width: '4vw',
+          zIndex: 9999,
+          bottom: '30%',
+          right: 250,
+          transform: 'translateX(0px) translateY(0px) translateZ(0px)',
+          transition: '0.1s',
+        }}
+        className="pokemon"
+      >
+        <Pokemon
+          width={'100%'}
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/134.gif"
+        ></Pokemon>
+      </motion.div>
+      <motion.div
+        drag
+        dragControls={dragControls}
+        animate={animationControls} // 애니메이션 컨트롤 적용
+        style={{
+          position: 'fixed',
+          display: 'inline-block',
+          width: '4vw',
+          zIndex: 9999,
+          bottom: '30%',
+          right: 350,
+          transform: 'translateX(0px) translateY(0px) translateZ(0px)',
+          transition: '0.1s',
+        }}
+        className="pokemon"
+      >
+        <Pokemon
+          width={'100%'}
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/149.gif"
+        ></Pokemon>
+      </motion.div>
+      <motion.div
+        drag
+        dragControls={dragControls}
+        animate={animationControls} // 애니메이션 컨트롤 적용
+        style={{
+          position: 'fixed',
+          display: 'inline-block',
+          width: '4vw',
+          zIndex: 9999,
+          bottom: '30%',
+          right: 400,
+          transform: 'translateX(0px) translateY(0px) translateZ(0px)',
+          transition: '0.1s',
+        }}
+        className="pokemon"
+      >
+        <Pokemon
+          width={'100%'}
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/150.gif"
+        ></Pokemon>
+      </motion.div>
+      <motion.div
+        drag
+        dragControls={dragControls}
+        animate={animationControls} // 애니메이션 컨트롤 적용
+        style={{
+          position: 'fixed',
+          display: 'inline-block',
+          width: '4vw',
+          zIndex: 9999,
+          bottom: '30%',
+          right: 300,
+          transform: 'translateX(0px) translateY(0px) translateZ(0px)',
+          transition: '0.1s',
+        }}
+        className="pokemon"
+      >
+        <Pokemon
+          width={'100%'}
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/151.gif"
+        ></Pokemon>
+      </motion.div>
+      <motion.div
+        drag
+        dragControls={dragControls}
+        animate={animationControls} // 애니메이션 컨트롤 적용
+        style={{
+          position: 'fixed',
+          display: 'inline-block',
+          width: '4vw',
+          zIndex: 9999,
+          bottom: '30%',
+          right: 450,
+          transform: 'translateX(0px) translateY(0px) translateZ(0px)',
+          transition: '0.1s',
+        }}
+        className="pokemon"
+      >
+        <Pokemon
+          width={'100%'}
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/135.gif"
+        ></Pokemon>
+      </motion.div>
       <Container ref={containerRef}>
-        <Tab style={{ width: `${width}%` }}>
+        <Tab width={width}>
           <div
             style={{
               width: '100%',
-              height: '80%',
+              height: '100%',
               overflow: 'auto',
             }}
           >
             <img src={problem1011} width={'100%'} alt="" />
           </div>
-          <Home onClick={handleDivClick}>
-            <motion.div
-              animate={controls}
-              style={{
-                position: 'relative',
-                transform: 'translate(50%, 50%)',
-                top: '50%',
-                left: position.x,
-                display: 'inline-block',
-                width: '4vw',
-                transition: '1s',
-              }}
-              className="pokemon"
-            >
-              <Pokemon
-                width={'100%'}
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/132.gif"
-              ></Pokemon>
-            </motion.div>
-          </Home>
         </Tab>
         <Resizer onMouseDown={handleMouseDown} style={{ left: width + '%' }} />
         <div
@@ -209,28 +316,21 @@ const ResizableTabsReview: React.FC = () => {
               overflow: 'auto',
             }}
           >
-            <img src={problem1011} width={'100%'} alt="" />
+            <Home>
+              <DesignedButton1 color="rgb(65, 0, 109)" onClick={handleDragEnd}>
+                원 위치로
+              </DesignedButton1>
+            </Home>
           </div>
-          <Home onClick={handleDivClick}>
-            <motion.div
-              animate={controls}
-              style={{
-                position: 'relative',
-                transform: 'translate(50%, 50%)',
-                top: '50%',
-                left: position.x,
-                display: 'inline-block',
-                width: '4vw',
-                transition: '1s',
-              }}
-              className="pokemon"
-            >
-              <Pokemon
-                width={'100%'}
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/132.gif"
-              ></Pokemon>
-            </motion.div>
-          </Home>
+          <div
+            style={{
+              width: '100%',
+              height: '20%',
+              overflow: 'auto',
+            }}
+          >
+            <img src={webrtc} width={'100%'} alt="" />
+          </div>
         </Tab>
       </Container>
     </motion.div>
@@ -238,7 +338,7 @@ const ResizableTabsReview: React.FC = () => {
 };
 const Home = styled.div`
   width: 100%;
-  height: 20%;
+  height: 100%;
   margin: 0 auto;
   background: url(${background});
   overflow: hidden;
