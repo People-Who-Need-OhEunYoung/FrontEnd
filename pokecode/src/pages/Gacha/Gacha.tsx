4255742059feb2a37b2790a024474e97e0360bab
Deokgi-Park <ejrrl6931@naver.com>
2024-07-12 17:14:37 +0900

    feat:뽑기 구현

--- pokecode/src/pages/Gacha/Gacha.tsx
+++ pokecode/src/pages/Gacha/Gacha.tsx
@@ -3,7 +3,40 @@ import { MainWrapper } from '../../components/MainWrapper';
 import garchimg from '../../assets/images/gachaimg.gif';
 import { DesignedButton1 } from '../../components/DesignedButton';
 import { Link } from 'react-router-dom';
+import gacha1 from '../../assets/images/가챠1.gif';
+import gacha2 from '../../assets/images/가챠2.gif';
+import gacha3 from '../../assets/images/가챠3.gif';
+import gacha4 from '../../assets/images/가챠4.gif';
+import gacha5 from '../../assets/images/가챠5.gif';
+import gacha6 from '../../assets/images/가챠6.gif';
+import { useState } from 'react';
+import { getGachaPokemon } from '../../utils/api/api';
+import { pokemonName } from '../../utils/api/api';
+import { updateMyPokemon } from '../../utils/api/api';
+
+const gachaArray = [gacha1, gacha2, gacha3, gacha4, gacha5, gacha6];
 const Gacha = () => {
+  const [background, setBackground] = useState(gachaArray[0]);
+  const [gachaRun, setGachaRun] = useState(false);
+  const [gachaResult, setGachaResult] = useState(false);
+  const [getPokemon, setGetPokemon] = useState(0);
+  const [getpokemonName, setGetpokemonName] = useState(0);
+
+  const gachaRunning = async () => {
+    const pokemonObj = await getGachaPokemon().then((res) => {
+      return res;
+    });
+    if (pokemonObj.result == 'fail') {
+      alert('크래딧 부족입니다.');
+      setGachaRun(false);
+      return;
+    }
+    setTimeout(async () => {
+      setGetPokemon(pokemonObj.poke_id);
+      setGetpokemonName(await pokemonName(pokemonObj.poke_id));
+      setGachaResult(true);
+    }, 3000);
+  };
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -31,6 +64,11 @@ const Gacha = () => {
           <DesignedButton1
             style={{ width: '400px', margin: '20px 0' }}
             color="#80495C"
+            onClick={() => {
+              setGachaRun(true);
+              setBackground(gachaArray[Math.floor(Math.random() * 6)]);
+              gachaRunning();
+            }}
           >
             뽑기 크래딧 (100)
           </DesignedButton1>
@@ -42,6 +80,85 @@ const Gacha = () => {
           >
             <Link to={'/usermain'}>나가기</Link>
           </DesignedButton1>
+        </div>
+        <div>
+          <MainWrapper
+            style={{
+              display: `${gachaRun ? 'block' : 'none'}`,
+              position: 'fixed',
+              top: '50%',
+              left: '50%',
+              width: '450px',
+              height: '400px',
+              transform: 'translate(-50%, -50%)',
+              backgroundImage: `url(${background})`,
+              backgroundSize: 'cover',
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center',
+            }}
+          ></MainWrapper>
+        </div>
+        <div>
+          <MainWrapper
+            style={{
+              display: `${gachaResult ? 'block' : 'none'}`,
+              position: 'fixed',
+              top: '50%',
+              left: '50%',
+              width: '450px',
+              height: '400px',
+              transform: 'translate(-50%, -50%)',
+              textAlign: 'center',
+            }}
+          >
+            <span
+              style={{
+                position: 'absolute',
+                top: '10px',
+                right: '5%',
+                fontSize: '2em',
+                cursor: 'pointer',
+              }}
+              onClick={() => {
+                setGachaRun(false);
+                setGachaResult(false);
+              }}
+            >
+              X
+            </span>
+            <h1 style={{ paddingTop: '40px' }}>다음 포켓몬이 뽑혔습니다.</h1>
+            <img
+              style={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%,-50%)',
+                width: '30%',
+              }}
+              src={
+                'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/' +
+                getPokemon +
+                '.gif'
+              }
+            />
+            <h1 style={{ position: 'absolute', bottom: '80px', width: '100%' }}>
+              {getpokemonName}
+            </h1>
+            <DesignedButton1
+              color=""
+              style={{
+                position: 'absolute',
+                left: '0',
+                bottom: '30px',
+                width: '80%',
+                boxSizing: 'border-box',
+                margin: '0 10%',
+              }}
+              onClick={() => updateMyPokemon(getPokemon)}
+            >
+              현재 포켓몬으로 변경
+            </DesignedButton1>
+          </MainWrapper>
         </div>
       </MainWrapper>
     </motion.div>
