65f5680b5479d0c9c117b9eca8f4898deabdccbe
hhyy0179 <rharhazbvl@gmail.com>
2024-07-25 05:23:41 +0900

    feat: 테스트케이스 수정중

--- pokecode/src/components/CodeRunButton/CodeRunButton.tsx
+++ pokecode/src/components/CodeRunButton/CodeRunButton.tsx
@@ -29,10 +29,13 @@ const CodeRunButton = () => {
   // }, [TestCases]);
 
   const handleSubmit = async () => {
-    const editorContent = writtenCode;
+    let editorContent = writtenCode;
     console.log('editorContent:', editorContent);
+    if (!editorContent) {
+      editorContent = ' ';
+    }
     try {
-      const response1 = await fetch(`http://192.168.1.18:3000/submit`, {
+      const response = await fetch(`http://192.168.1.18:3000/submit`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -47,35 +50,15 @@ const CodeRunButton = () => {
           testCase: TestCases,
         }),
       })
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-      });
-
-      // const response = await fetch(`${import.meta.env.VITE_APP_IP}/runCode`, {
-      //   method: 'POST',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //     Authorization: `Bearer ${localStorage.getItem('token')}`,
-      //   },
-      //   body: JSON.stringify({
-      //     code: editorContent,
-      //     bojNumber: id,
-      //     elapsed_time: elapsedTime,
-      //     limit_time: limitTime,
-      //     testCase: TestCases,
-      //   }),
-      // });
-
-      // if (!response.ok) {
-      //   throw new Error(`Error: ${response.statusText}`);
-      // }
-
-      // const data = await response.json();
-      // dispatch(setReturnCall(data.data2));
-      // console.log(data.data2);
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          const dataArray = data.result.results;
+          const resultString = dataArray.map((item: any) => item.result).join('\n\n');
+          console.log(resultString);
+          dispatch(setReturnCall(resultString));
+        });
     } catch (error) {
       console.error('테스트 케이스 과정 에러 발생 : ', error);
     }
