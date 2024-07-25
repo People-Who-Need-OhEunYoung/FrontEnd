899782c416cb2745ba5a9a4d50bc22e1e7887294
hhyy0179 <rharhazbvl@gmail.com>
2024-07-26 03:12:38 +0900

    feat: 버튼 바꾸기

--- pokecode/src/components/ResizableTabsReview/VoiceChatOV.tsx
+++ pokecode/src/components/ResizableTabsReview/VoiceChatOV.tsx
@@ -184,7 +184,7 @@ const VoiceChatOV: React.FC = () => {
       {isJoined && publisher ? (
         <div
           style={{
-            height: 'calc(100% - 80px)',
+            height: '100%',
             overflowY: 'auto',
             width: '100%',
           }}
